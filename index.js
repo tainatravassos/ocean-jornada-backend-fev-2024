90e87f1b0fdbed1be7ae1aba4b3aadd667ca197f
tainatravassos <118690705+tainatravassos@users.noreply.github.com>
2024-02-20 14:41:26 +0000

    express instalado

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hello, world!")
+const express = require('express')
+
+const app = express()
+
+ 
+
+app.get('/', function (req, res) {
+
+  res.send('Hello World')
+
+})
+
+ 
+
+app.listen(3000)
+
